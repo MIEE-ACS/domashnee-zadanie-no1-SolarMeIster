b12155b926cde6843191fe06f841a247c0bce82b
SolarMeIster <71086378+SolarMeIster@users.noreply.github.com>
2020-09-12 12:40:11 +0300

    задание вроде сделано

---
+++ HomeWork_1/HomeWork_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            decimal number = decimal.Parse(Console.ReadLine());
+            decimal first = number * number; // вторая степень 
+            decimal second = first * number; // третья степень
+            first = second * first; // пятая степень
+            second = first * first; // десятая степень
+            second = first * second; // пятнадцатая степень
+            Console.WriteLine($"Пятнадцатая степень числа {number}: {second}");
+        }
+    }
+}
